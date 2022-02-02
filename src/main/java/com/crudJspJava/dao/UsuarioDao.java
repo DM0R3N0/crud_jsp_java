6dc22168bdd1c6921944e5b38cc80eef7a2f3975
DM0R3N0 <dfm84@hotmail.com>
2022-02-02 15:34:52 -0300

    Initial Commit

---
+++ src/main/java/com/crudJspJava/dao/UsuarioDao.java
@@ -0,0 +1,65 @@
+package com.crudJspJava.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.crudJspJava.bean.Usuario;
+
+
+public class UsuarioDao {
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crudjspjava","root","sophia01");
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+
+
+
+
+public static List<Usuario> getAllUsuarios(){
+	
+	//String sql = "SELECT * FROM contatos";
+	
+	List<Usuario> list = new ArrayList<Usuario>();
+
+	try {
+	Connection con = null;
+	PreparedStatement ps = con.prepareStatement("SELECT * FROM usuario");
+	//Classe que vai recuperar os dados do banco. ***SELECT****
+	ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+			
+			Usuario usuario = new Usuario();
+			
+			//Recuperar o id
+			usuario.setId(rs.getInt("id"));
+			//Recuperar o nome
+			usuario.setNome(rs.getString("nome"));
+			//Recuperar o password
+			usuario.setPassword(rs.getString("password"));
+			//Recuperar o sexo
+			usuario.setSexo(rs.getString("sexo"));
+			//Recuperar o país
+			usuario.setPais(rs.getString("pais"));
+			
+			list.add(usuario);
+			
+		}
+	}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+}
+}
---
+++ src/main/webapp/viewusuarios.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Visualização de usuários</title>
+</head>
+<body>
+	
+	<%@ page import="com.crudJspJava.dao.UsuarioDao, com.crudJspJava.bean.*, java.util.*"%>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+		
+	<h1>Listagem de usuários</h1>	
+	
+	<%
+	List<Usuario> list = UsuarioDao.getAllUsuarios();
+	request.setAttribute("list", list);
+	%>
+	
+	<table>
+		<tr><th>ID</th><th>Nome</th><th>Password</th><th>Email</th><th>Sexo</th><th>País</th></tr>
+		
+		<c:forEach items="${list}" var="usuario">
+			<tr>
+				<td>${usuario.getId()}</td>
+				<td>${usuario.getNome()}</td>
+				<td>${usuario.getPassword()}</td>
+				<td>${usuario.getSexo()}</td>
+				<td>${usuario.getPais()}</td>
+			</tr>		
+		</c:forEach>		
+	</table>
+
+</body>
+</html>
